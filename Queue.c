96a2954ccab572205a0b4db9514988c037a6ff6c
Rani Jain <88179476+Jainrani@users.noreply.github.com>
2024-01-13 21:37:26 +0530

    Create Queue.c

---
+++ Queue.c
@@ -0,0 +1,123 @@
+#include<stdio.h>
+
+#include<conio.h>
+
+#define max 5
+
+typedef struct Queue
+
+{
+
+ int data[max];
+
+ int front,rear;
+
+}s;
+
+s s1,*p;
+
+void init()
+
+{
+
+  p=&s1;
+
+  p->front=p->rear=-1 ;
+
+}
+
+int Empty()
+
+{
+
+  if(p->front==-1)
+
+  return 1;
+
+  else
+
+  return 0;
+
+}
+
+int Full()
+
+{
+
+  if(p->rear==max-1)
+
+  return 1;
+
+  else
+
+  return 0;
+
+}
+
+void Enq(int x)
+
+{
+
+  if(Full()==1)
+
+  return;
+
+  p->rear++;
+
+  p->data[p->rear]=x;
+
+  if(p->front==-1)
+
+  p->front=0;
+
+}
+
+int Deq()
+
+{
+
+  int x;
+
+  if(Empty()==1)
+
+  return;
+
+   x=p->data[(p->front)];
+
+   if(p->front==p->rear)
+
+   p->front=p->rear=-1;
+
+   else
+
+   p->front++;
+
+   return x ;
+
+
+
+}
+
+void main()
+
+{
+
+clrscr();
+
+init();
+
+Enq(10);
+
+Enq(20);
+
+Enq(30);
+
+printf(" \n%d",Deq());
+
+printf(" \n%d",Deq());
+
+printf(" \n%d",Deq());
+
+getch();
+}
+
